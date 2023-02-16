1497f1328b85c0a8e077479890e9d63ac656cd15
James <jamespham93@yahoo.com>
2023-02-15 16:37:11 -0800

    Init commit

---
+++ todo-react-admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: TODO React Admin
+ * Plugin URI: https://github.com/vta/vta-wc-custom-order-status/
+ * Description: A WordPress-React tutorial on for wp-admin.
+ * Version: 0.1
+ * Author: James Pham
+ */
+
+function runPlugin(): void {
+    
+}
